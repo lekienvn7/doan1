46d619926f7df1e8bdbe7407ca21f484fe7ac76a
lekienvn7 <ltrgkien68.68@gmail.com>
2025-09-26 20:14:27 +0700

    them note

--- script/forgot.js
+++ script/forgot.js
@@ -1,5 +1,11 @@
-const loginBtn = document.getElementById("btn-login");
-const completeText = document.querySelector(".complete-text");
+const loginBtn =
+  document.getElementById(
+    "btn-login"
+  ); /* Lấy button của file forgot.html qua id */
+const completeText =
+  document.querySelector(
+    ".complete-text"
+  ); /* Lấy div chứa chữ thông báo bằng class complete-text */
 
 loginBtn.addEventListener("click", () => {
   completeText.classList.add("active");
